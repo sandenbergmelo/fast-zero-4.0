28fd7384b31bace034b430c164837c89042f89ea
Sandenberg Ferreira Melo <77549672+sandenbergmelo@users.noreply.github.com>
2025-05-21 22:03:27 -0300

    Add database integration with SQLAlchemy and migrations with alembic

---
+++ tests/test_db.py
@@ -0,0 +1,24 @@
+from dataclasses import asdict
+
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from fast_zero.db.models import User
+
+
+def test_create_user(session: Session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        user = User(username='test', email='test@test.com', password='secret')
+
+        session.add(user)
+        session.commit()
+
+        fetched_user = session.scalar(select(User).where(User.id == 1))
+
+    assert asdict(fetched_user) == {  # type: ignore
+        'id': 1,
+        'username': 'test',
+        'email': 'test@test.com',
+        'password': 'secret',
+        'created_at': time,
+    }
